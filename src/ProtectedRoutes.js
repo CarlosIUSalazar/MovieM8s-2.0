225e3d2c58e0d0a743d72d4dce9c4e9916dc07bd
Carlos Salazar <carlosiusalazar@gmail.com>
2020-07-04 22:00:03 +0900

    Added table needs navigation

--- src/ProtectedRoutes.js
+++ src/ProtectedRoutes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReportsView from './ReportsView'
+import Table from './components/Table'
 
 const protectedRoutes = [
 	{
@@ -7,6 +8,13 @@ const protectedRoutes = [
 		exact: true,
 		path: '/reports',
 		main: props => <ReportsView {...props} />,
+		public: false,
+	},
+	{
+		name: 'table',
+		exact: true,
+		path: '/table',
+		main: props => <Table {...props} />,
 		public: false,
 	},
 ];
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Login from "./Login";
 import Join from "./Join";
+import Table from "./components/Table"
 
 const routes = [
   { name: "Login", path: "/", exact: true, main: () => <Login /> },
   { name: "Join", path: "/login", exact: true, main: () => <Join /> }
+  //{ name: "Table", path: "/table", exact: true, main: () => <Table /> }
+
 ];
 
 export default routes;
